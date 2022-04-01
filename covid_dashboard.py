f001550f048938376fdeb62744edcb428b19a1d9
mysisteristhebest <chifaawehbe@gmail.com>
2022-04-01 15:11:01 +0200

    csv

--- covid_dashboard.py
+++ covid_dashboard.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import streamlit as st
+import matplotlib as plt
 
-confirmed = pd.read_csv('time_series_covid19_confirmed_global.csv')
-death = pd.read_csv('time_series_covid19_deaths_global.csv')
-recovered = pd.read_csv('time_series_covid19_recovered_global.csv')
+confirmed = pd.read_csv('time_series_covid19_confirmed_global.csv', on_bad_lines = 'skip')
+death = pd.read_csv('time_series_covid19_deaths_global.csv', on_bad_lines = 'skip')
+recovered = pd.read_csv('time_series_covid19_recovered_global.csv', on_bad_lines = 'skip')
+
+print(confirmed.head(5))
