03136045db25d080543c5559013ff348d96c699f
mysisteristhebest <chifaawehbe@gmail.com>
2022-04-01 16:07:53 +0200

    first try

--- covid_dashboard.py
+++ covid_dashboard.py
@@ -6,4 +6,20 @@ confirmed = pd.read_csv('time_series_covid19_confirmed_global.csv', on_bad_lines
 death = pd.read_csv('time_series_covid19_deaths_global.csv', on_bad_lines = 'skip')
 recovered = pd.read_csv('time_series_covid19_recovered_global.csv', on_bad_lines = 'skip')
 
-print(confirmed.head(5))
+st.sidebar.checkbox("Show Analysis by State", True, key=1)
+select = st.sidebar.selectbox('Select a State',confirmed['Country/Region'])
+
+def simpleGraph():
+  fig=plt.figure(figsize=(14,6))
+  plt.title("Death toll")
+  plt.xticks(rotation=90)
+  plt.xlabel("Date", fontsize=8)
+  plt.ylabel("Total deaths per million", fontsize=8)
+  sns.lineplot(data=data['total_deaths'])
+  return fig
+
+
+
+# print(confirmed.head(5))
+# print(death.head(5))
+# print(recovered.head(5))
