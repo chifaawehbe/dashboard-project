b9ee9468128c03675263eb3c5dc06d3869b69748
mysisteristhebest <chifaawehbe@gmail.com>
2022-04-01 14:45:19 +0200

    covid dashboard

---
+++ covid_dashboard.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+confirmed = pd.read_csv('time_series_covid19_confirmed_global.csv')
+death = pd.read_csv('time_series_covid19_deaths_global.csv')
+recovered = pd.read_csv('time_series_covid19_recovered_global.csv')
