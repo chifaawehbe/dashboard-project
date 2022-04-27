4f5221280adc0c1847426667bfdc5369bb27b4e2
khattaksaab <abdullahkhattak@gmail.com>
2022-04-27 16:27:14 +0200

    testing commit

--- abdullah_covid_viz.py
+++ abdullah_covid_viz.py
@@ -6,86 +6,89 @@ import plotly.graph_objects as go
 import plotly.express as px
 import datetime
 
-st.title('COVID dashboard')
+st.title('COVID Dashboard')
 st.markdown('''Hello :wave:  For our *open-source* project, we created an interactive **Covid-19** dashboard 
     that allows users to visualize the number of  Covid-19 cases or deaths per country 
     as a function of time''')
 
-st.markdown('''
-          Coronavirus disease (COVID-19) is an infectious disease caused by a newly 
-            discovered coronavirus. Most people infected with the COVID-19 virus will 
-            experience mild to moderate respiratory illness and recover without requiring 
-            special treatment.''')
+# st.markdown('''
+#           Coronavirus disease (COVID-19) is an infectious disease caused by a newly 
+#             discovered coronavirus. Most people infected with the COVID-19 virus will 
+#             experience mild to moderate respiratory illness and recover without requiring 
+#             special treatment.''')
 
 
-#making sidebar            
-st.sidebar.title("visualization")
-
-# loading data into dataframe
-covid = pd.read_csv('owid-covid-data.csv')
-# print(covid.head)
-
-st.write('OWID COVID DATASET')
-
-# st.dataframe(covid)
+            
+st.sidebar.title("Visulization selector") #making sidebar
 
 
-# @st.cache
-# def load_data():
-#   covid = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
-#   covid['date'] = pd.to_datetime(covid['date'])
-#   return covid
+covid = pd.read_csv('owid-covid-data.csv') #loading data through CSV
 
-# # function to build a plot
-# def plot():
-  
-    # covid = load_data()
+# st.write('OWID COVID DATASET')
 
-    #datepicker
-today = datetime.date.today() #to get todays date
+
+########## DATE SELECTOR #############
 
 start_date = st.sidebar.date_input('Start date', datetime.date(2020,3,1)) #dateselecter for start date
-tomorrow = today + datetime.timedelta(days=1)
+
+today = datetime.date.today() #to get todays date 
+
+tomorrow = today + datetime.timedelta(days=1) #tomorrows date
+
 end_date = st.sidebar.date_input('End date', tomorrow) #dataselecter for end date, it has tomorrow because end date is always ahead
 
-if start_date < end_date:
-    st.success('Start date: `%s`\n\nEnd date: `%s`' % (start_date, end_date))
-else:
-    st.error('Error: End date must fall after start date.')
-
-covid["date"] = pd.to_datetime(covid["date"]).dt.date
-mask = (covid['date']< end_date) & (covid['date'] >= start_date)
-covid2=covid[mask]
+# if start_date < end_date:
+#     st.success('Start date: `%s`\n\nEnd date: `%s`' % (start_date, end_date))
+# else:
+#     st.error('Error: End date must fall after start date.')
 
 
-st.dataframe(covid)
+covid["date"] = pd.to_datetime(covid["date"]) # converting covid[data] to a datetime format
 
-clist = covid2['location'].unique().tolist()
+covid['date'] = covid['date'].dt.date # converting covid[data] to a datetime format 
 
-countries = st.multiselect("Select country", clist, default = 'France')
-st.header("You selected: {}".format(", ".join(countries)))
+mask = (covid['date']< end_date) & (covid['date'] >= start_date) #creating a filter for the dates that will be selected
 
-dfs = {country: covid2[covid2["location"] == country] for country in countries}
+covid2=covid[mask] # new date filter dateframe
 
-fig = go.Figure()
-for country, covid2 in dfs.items():
-    fig = fig.add_trace(go.Scatter(x=covid2["date"], y=covid2["total_cases"], name=country))
+##############################################
 
-st.plotly_chart(fig)
+# st.dataframe(covid2) #viewing dataframe on dashbaord
+
+clist = covid2['location'].unique().tolist() #unique list of countries
+
+countries = st.multiselect("Select Country", clist, default = 'France') # creating a multiselector for countries, DEFAULT country 'France'
+st.header("You selected: {}".format (", ".join(countries))) # header to print what countries selected
+
+dfs = {country: covid2[covid2["location"] == country] for country in countries} #making dictionary of dataframe based on country selected
+
+page = st.sidebar.selectbox('Selected Option',('Total cases', 'Total deaths')) #select box in sidebar
+
+rad = st.sidebar.radio('select option', ['7 Day Rolling Average','Raw Number', 'Cummulative Number'])
+
+diff_deaths = covid2['total_deaths'].diff() # have to difference between rows to calculate rolling average
+diff_cases = covid2['total_cases'].diff()
+
+deaths_roll = diff_deaths.rolling(window = 7).mean() #7 day rolling average
+cases_roll = diff_cases.rolling(window = 7).mean()
 
 
-
-page = st.sidebar.selectbox('Total cases',('Total cases', 'Total deaths'))
 if page == 'Total cases':
-    st.title("Total cases by country")   
-    # plot()
 
-#  else:
-#     st.title("Total cases in Oceania") 
-#     ocean = covid.loc[covid['continent'] == "Oceania"] 
-#     fig = px.line(ocean, x="date", y='total_cases')
-#     st.plotly_chart(fig)
+    if rad == '7 Day Rolling Average':
+        st.write("7 day rolling average of Cases")
+        fig = go.Figure()
+        for country, covid2 in dfs.items():
+            fig = fig.add_trace(go.Scatter(x=covid2["date"], y= cases_roll, name= country))
+        st.plotly_chart(fig)
 
+if page == 'Total deaths':
 
-# cumulative, raw, smooth
-# normalized data
+    if rad == '7 Day Rolling Average':
+        st.write("7 day rolling average of Deaths")
+        fig = go.Figure()
+        for country, covid2 in dfs.items():
+            fig = fig.add_trace(go.Scatter(x=covid2["date"], y= deaths_roll, name= country))
+        st.plotly_chart(fig)
+
+# agoifdjfadslifjdslf;asjfslkfjsflkfjsalfksddd
